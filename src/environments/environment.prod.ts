96caa2077582ca44b0ee7c32c57d532911477d4c
Manish Kumar Gurjar <53900987+manishkumar4592@users.noreply.github.com>
2019-08-28 14:08:31 +0530

    Changes by Manish

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,15 +1,15 @@
 //import { firebaseConfig } from '../environments/firebase-config'; // please comment on this line when changing the values ​​of firebase {}
 export const environment = {
   production: true,
-  version: require('../../package.json').version, // https://stackoverflow.com/questions/34907682/how-to-display-app-version-in-angular2
+  version: require('../../package.json').version, // https://stackoverflow.com/questions/349076002/how-to-display-app-version-in-angular2
   remoteConfig: false, // for performance don't load settings from remote
   firebase: {
-    apiKey:"AIzaSyDvQaRqiSt8cJMwJ_TVLomgMbeAaBnjm0I",
-    authDomain: "scoonews.firebaseapp.com",
-    databaseURL: "https://scoonews.firebaseio.com",
-    projectId: "scoonews",
-    storageBucket: "scoonews.appspot.com",
-    messagingSenderId: "382632848826"
+    apiKey:"AIzaSyDvQaRqiSt00cJMwJ_TVLomgMbeAaBnjm0I",
+    authDomain: "news.firebaseapp.com",
+    databaseURL: "https://news.firebaseio.com",
+    projectId: "news",
+    storageBucket: "news.appspot.com",
+    messagingSenderId: "3002632004000026"
   },
   apiUrl: 'https://api.tiledesk.com/v1/',
   tenant: 'tilechat',
